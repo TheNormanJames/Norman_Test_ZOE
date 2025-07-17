fbc93d9dd6da724fb26cc40111a434766c9ae435
NormanJaimes <normane_@hotmail.com>
2025-07-17 17:23:17 -0500

    feat: :sparkles: add: Paginator with URLparams

--- src/components/dashboard/AdvisorTable.tsx
+++ src/components/dashboard/AdvisorTable.tsx
@@ -12,7 +12,7 @@ type AdvisorTableProps = {
   income: number;
 };
 
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 3;
 
 export default function AdvisorTable({ advisors, income }: AdvisorTableProps) {
   const router = useRouter();
@@ -22,16 +22,19 @@ export default function AdvisorTable({ advisors, income }: AdvisorTableProps) {
     key: 'name',
     direction: 'asc',
   });
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     setTypeSort({
       key: paramsURL.get('sort') || 'name',
       direction: paramsURL.get('order') || 'asc',
     });
+    setCurrentPage(parseInt(paramsURL.get('page') || '1'));
   }, []);
 
   const totalPages = Math.ceil(advisors.length / ITEMS_PER_PAGE);
-  console.log(totalPages);
+  const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
+  // console.log(pageNumbers);
 
   const handleSearch = (text: string) => {
     setSearchParams(text);
@@ -63,13 +66,21 @@ export default function AdvisorTable({ advisors, income }: AdvisorTableProps) {
         advisor.income.toString().includes(searchParams.toLowerCase())
     );
 
-    return result;
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+    const endIndex = startIndex + ITEMS_PER_PAGE;
+
+    return result.slice(startIndex, endIndex);
   };
-  const updateParams = () => {
+  const updateParams = (newParams: Record<string, string>) => {
     const params = new URLSearchParams(paramsURL.toString());
-    params.set('order', typeSort.direction);
-    params.set('sort', typeSort.key);
+    // params.set('order', typeSort.direction);
+    // params.set('sort', typeSort.key);
+
+    Object.entries(newParams).forEach(([Key, value]) => {
+      params.set(Key, value);
+    });
     router.push(`/advisors?${params.toString()}`);
+
     // console.log(params.toString());
   };
   const handleSort = (key: keyof Advisor) => {
@@ -77,8 +88,18 @@ export default function AdvisorTable({ advisors, income }: AdvisorTableProps) {
       key: key,
       direction: typeSort.direction === 'asc' ? 'desc' : 'asc',
     });
-    updateParams();
+    updateParams({ order: typeSort.direction, sort: typeSort.key });
   };
+
+  const handlePageChange = (currentPage: number) => {
+    setCurrentPage(currentPage);
+    updateParams({ page: currentPage.toString() });
+  };
+
+  const currentAdvisors =
+    currentPage * ITEMS_PER_PAGE > advisors.length
+      ? advisors.length
+      : currentPage * ITEMS_PER_PAGE;
   return (
     <div className={styles.container}>
       <div className={styles.topBarMain}>
@@ -133,24 +154,40 @@ export default function AdvisorTable({ advisors, income }: AdvisorTableProps) {
       </div>
       <div className={styles.paginationContainer}>
         <div className={styles.paginationInfo}>
-          <b>10 of 34</b> Advisors
+          <b>
+            {currentAdvisors} of {advisors.length}
+          </b>{' '}
+          Advisors
         </div>
         <div className={styles.paginationButtons}>
-          <button className={styles.paginationArrow}>&#8676;</button>
+          <button
+            className={styles.paginationArrow}
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            &#8676;
+          </button>
           <div className={styles.pageNumbers}>
-            {totalPages}
-            <button
-              key="first"
-              className={`${styles.paginationNumber} ${styles.activePage}`}
-              id="Btn1"
-            >
-              1
-            </button>
-            <button key="second" className={styles.paginationNumber} id="Btn2">
-              2
-            </button>
+            {pageNumbers.map((pageNumber) => (
+              <button
+                key={pageNumber}
+                className={`${styles.paginationNumber} ${
+                  pageNumber === currentPage ? styles.activePage : ''
+                }`}
+                id="Btn1"
+                onClick={() => handlePageChange(pageNumber)}
+              >
+                {pageNumber}
+              </button>
+            ))}
           </div>
-          <button className={styles.paginationArrow}>&#8677;</button>
+          <button
+            className={styles.paginationArrow}
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            &#8677;
+          </button>
         </div>
       </div>
     </div>
